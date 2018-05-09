10e32083aa8442b372e7269cb9fa250ad3bdfd6d
Moises Ortega <mortega@localhost>
2018-05-08 20:10:43 -0500

    master

---
+++ rsvp_umbrella/apps/rsvp/lib/rsvp.ex
@@ -0,0 +1,15 @@
+defmodule Rsvp do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+      children =[
+        worker(Rsvp.Repo, [])
+      ]
+
+      opts=[strategy: :one_for_one, name: Rsvp.Supervisor]
+      Supervisor.start_link(children, opts)
+  end
+
+end
---
+++ rsvp_umbrella/apps/rsvp/priv/repo/migrations/20180508232915_add_events_table.exs
@@ -0,0 +1,7 @@
+defmodule Rsvp.Repo.Migrations.AddEventsTable do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
