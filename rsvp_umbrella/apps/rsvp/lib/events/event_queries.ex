5ff186a05bffadf8b0195e8799e79d644eca3028
Moises Ortega <mortega@innovacion.net>
2018-05-09 14:45:24 -0500

    cambios3

--- rsvp_umbrella/apps/rsvp/lib/events/event_queries.ex
+++ rsvp_umbrella/apps/rsvp/lib/events/event_queries.ex
@@ -7,4 +7,15 @@ defmodule Rsvp.EventQueries do
     Repo.all(from Events)
   end
 
+  def get_all_for_location(location) do
+      query = from e in Events,
+      where: e.location ==^location
+
+      Repo.all(query)
+  end
+
+  def get_by_id(id) do
+      Repo.get(Events, id)
+  end
+
 end
